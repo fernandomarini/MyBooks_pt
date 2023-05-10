cd6c09d1f79c7a06b84ac4b629c68d14087a0cbe
Fer Ms <86858568+fernandomarini@users.noreply.github.com>
2023-05-10 06:21:31 -0300

    Revert "Dia4.1"

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -16,14 +16,14 @@ export class BooksComponent {
     this.books = [];
 
     let book001 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://http2.mlstatic.com/D_NQ_NP_657353-MLA44342368528_122020-O.jpg",1,2);
-    let book002 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",2,);
-    let book003 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",3,5);
-    let book004 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://http2.mlstatic.com/D_NQ_NP_657353-MLA44342368528_122020-O.jpg",4,6);
-    let book005 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",5,2);
-    let book006 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",6,5);
-    let book007 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",7,2);
-    let book008 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",8,5);
-    let book009 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",9,2);
+    let book002 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",5,);
+    let book003 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",2,5);
+    let book004 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://http2.mlstatic.com/D_NQ_NP_657353-MLA44342368528_122020-O.jpg",2,6);
+    let book005 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144");
+    let book006 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",3,5);
+    let book007 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",1,2);
+    let book008 = new Book("Sombras","Ciencia Ficcion","E.King",19,"https://media.revistagq.com/photos/5f6c9b117458e06669aaa2b0/master/w_1643,h_2500,c_limit/6%20resplandor.jpg",3,5);
+    let book009 = new Book("La Bestia","Ciencia Ficcion","E.King",19,"https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg?w=144",1,2);
 
     this.books.push(book001);
     this.books.push(book002);
@@ -49,11 +49,6 @@ export class BooksComponent {
 
     };
 
-    public borrar( libroBorrar: Book){
-      let borrarBook = this.books.filter( books => books.id_book != libroBorrar.id_book)
-    
-      this.books = borrarBook;
-    }
 
 };
 
